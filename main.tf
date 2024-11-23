65926d005fd1e72426c2a2807233223024c11309
eduar <eduardofmanrique@outlook.com>
2024-11-23 17:23:24 -0300

    add main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
