94b2abc90aa5d0a06b0ed243f2d5d5db9876a4c9
eduar <eduardofmanrique@outlook.com>
2024-11-23 22:44:17 -0300

    try to add bucket versioning

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ provider "aws" {
 resource "aws_s3_bucket" "example" {
   bucket = "aws-crypto-report-example-bucket"
 }
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
