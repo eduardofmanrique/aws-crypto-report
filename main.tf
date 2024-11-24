2ff61af12130c093b4a43acc02470a30775c8d2a
eduar <eduardofmanrique@outlook.com>
2024-11-24 17:22:53 -0300

    buckets lower case

--- main.tf
+++ main.tf
@@ -11,11 +11,12 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-example-bucket-${random_string.suffix.result}"
-}
-
 resource "random_string" "suffix" {
   length  = 8
   special = false
+  upper   = false  # Ensure no uppercase letters
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket-${lower(random_string.suffix.result)}"
 }
