9a498baa216f42b86b18581ae3251016c54e8fc3
eduar <eduardofmanrique@outlook.com>
2025-01-07 20:20:17 -0300

    requirements.txt test

--- lambda/main.py
+++ lambda/main.py
@@ -1,5 +1,17 @@
+import pandas as pd
+
+
 def handler(event, context):
+    print(pd.DataFrame(
+        {
+            'A': [1,2,3],
+            'B': [3,4,5]
+        }
+    ))
     return {
         'statusCode': 200,
         'body': 'Dummy Lambda function executed! From git'
     }
+
+if __name__ == '__main__':
+    handler('', '')
