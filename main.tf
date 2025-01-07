056a787d02b33596635fe21b6d90399bdf661dad
eduar <eduardofmanrique@outlook.com>
2025-01-07 18:24:54 -0300

    test lambda creation

--- main.tf
+++ main.tf
@@ -11,32 +11,34 @@ terraform {
   }
 }
 
-# Generate random suffix for the first bucket
-resource "random_string" "suffix_1" {
-  length  = 10
-  special = false
-  upper   = false  # Ensure no uppercase letters
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-basic-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-# Generate random suffix for the second bucket
-resource "random_string" "suffix_2" {
-  length  = 10
-  special = false
-  upper   = false  # Ensure no uppercase letters
+resource "aws_iam_policy_attachment" "lambda_basic_policy" {
+  name       = "lambda-basic-policy-attachment"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Create the first S3 bucket
-resource "aws_s3_bucket" "example_1" {
-  bucket = "my-example-bucket-1-${lower(random_string.suffix_1.result)}"
-  acl    = "private"
+resource "aws_lambda_function" "example" {
+  function_name = "simple-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.9"
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
 }
-
-# Create the second S3 bucket
-resource "aws_s3_bucket" "example_2" {
-  bucket = "my-example-bucket-2-${lower(random_string.suffix_2.result)}"
-  acl    = "private"
-}
-
-#force update to test
-
 
