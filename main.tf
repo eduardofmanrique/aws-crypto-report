3fab1421ef2b839d6be4d4b40481ef93a69f66d3
eduar <eduardofmanrique@outlook.com>
2025-01-18 14:21:39 -0300

    aws_iam_policy_attachment change to aws_iam_policy_attachment

--- main.tf
+++ main.tf
@@ -27,38 +27,37 @@ resource "aws_iam_role" "lambda_role_crypto_report" {
   })
 }
 
-resource "aws_iam_policy_attachment" "lambda_basic_policy_crypto_report" {
-  name       = "lambda-basic-policy-attachment-crypto-report"
-  roles      = [aws_iam_role.lambda_role_crypto_report.name]
+resource "aws_iam_role_policy_attachment" "lambda_basic_policy_crypto_report" {
+  role       = aws_iam_role.lambda_role_crypto_report.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_policy_attachment" "lambda_sqs_full_access_crypto_report" {
-  name       = "lambda-sqs-full-access-attachment-crypto-report"
-  roles      = [aws_iam_role.lambda_role_crypto_report.name]
+resource "aws_iam_role_policy_attachment" "lambda_sqs_full_access_crypto_report" {
+  role       = aws_iam_role.lambda_role_crypto_report.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
 }
 
 resource "aws_lambda_layer_version" "dependencies_layer_crypto_report" {
-  filename   = "lambda_dependencies.zip"
-  layer_name = "lambda-dependencies-crypto-report"
+  filename            = "lambda_dependencies.zip"
+  layer_name          = "lambda-dependencies-crypto-report"
   compatible_runtimes = ["python3.9"]
 }
 
 resource "aws_lambda_function" "lambda_crypto_report" {
-  function_name = "lambda-crypto-report"
-  role          = aws_iam_role.lambda_role_crypto_report.arn
-  handler       = "main.handler"
-  runtime       = "python3.9"
-  filename      = "lambda.zip"
+  function_name    = "lambda-crypto-report"
+  role             = aws_iam_role.lambda_role_crypto_report.arn
+  handler          = "main.handler"
+  runtime          = "python3.9"
+  filename         = "lambda.zip"
   source_code_hash = filebase64sha256("lambda.zip")
-  timeout       = 120
-  memory_size   = 300
-  layers        = [
+  timeout          = 120
+  memory_size      = 300
+  layers           = [
     "arn:aws:lambda:sa-east-1:336392948345:layer:AWSSDKPandas-Python39:29",
     aws_lambda_layer_version.dependencies_layer_crypto_report.arn
   ]
 }
+
 
 resource "aws_cloudwatch_event_rule" "every_2_minutes_crypto_report" {
   name        = "lambda-trigger-every-2-minutes"
